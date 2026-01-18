39c64fef5fc3fd1653cfd4091bb00877f4b06ea8
mohamed2021041 <m01028761991@gmail.com>
2026-01-18 22:51:34 +0200

    add Theme

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:remindly/main.dart';
+import 'package:remindly/app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
