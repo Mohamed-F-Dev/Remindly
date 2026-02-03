196182be1eff38ccc25add6c7037e53060b98b51
mohamed2021041 <m01028761991@gmail.com>
2026-02-03 03:00:59 +0200

    add localcubit and getReminderuscase for di

--- lib/core/di/di.dart
+++ lib/core/di/di.dart
@@ -7,6 +7,8 @@ import 'package:remindly/domain/model/token_type_adapter.dart';
 import 'package:remindly/domain/parser/reminder_parser.dart';
 import 'package:remindly/domain/repo/reminder_repo.dart';
 import 'package:remindly/domain/usecase/add_reminder.dart';
+import 'package:remindly/domain/usecase/get_reminders.dart';
+import 'package:remindly/ui/bloc/local_reminder_cubit/local_reminder_cubit.dart';
 import 'package:remindly/ui/bloc/reminder_cubit/reminder_cubit.dart';
 
 final sl = GetIt.instance;
@@ -34,9 +36,15 @@ Future<void> initDI() async {
   sl.registerLazySingleton<AddReminderUseCase>(
     () => AddReminderUseCase(repo: sl<ReminderRepositories>()),
   );
+  sl.registerLazySingleton<GetRemindersUseCase>(
+    () => GetRemindersUseCase(sl<ReminderRepositoriesImpl>()),
+  );
 
   // cubit
   sl.registerFactory<ReminderCubit>(
     () => ReminderCubit(sl<AddReminderUseCase>()),
+  );
+  sl.registerFactory<LocalReminderCubit>(
+    () => LocalReminderCubit(sl<GetRemindersUseCase>()),
   );
 }
