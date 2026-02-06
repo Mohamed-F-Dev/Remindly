1268101e5112deb4c1592146273fd23a759ca556
mohamed2021041 <m01028761991@gmail.com>
2026-02-06 19:35:04 +0200

    add sve reminder Use Case

---
+++ lib/domain/usecase/save_reminder.dart
@@ -0,0 +1,11 @@
+import 'package:remindly/domain/model/parse_reminder.dart';
+import 'package:remindly/domain/repo/reminder_repo.dart';
+
+class SaveReminderUseCase {
+  final ReminderRepositories repo;
+  const SaveReminderUseCase(this.repo);
+
+  Future<void> call({required final ParsedReminder reminder}) async {
+    await repo.SaveReminder(reminder: reminder);
+  }
+}
